4d38a8491bedfad8546aae7791ade229e17af844
Jung Sung Hun <codingj87@zensystems.co.kr>
2022-01-07 10:07:12 +0900

    route app 에서 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import PostList from "./pages/post/PostList";
-import PostDetail from "./pages/post/PostDetail";
-
+import Routes from "./routes";
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="postList/:postId" element={<PostDetail />} />
-      </Routes>
-    </>
-  );
+  return <Routes />;
 }
 
 export default App;
